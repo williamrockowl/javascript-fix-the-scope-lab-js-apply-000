c55c7ac827e55f6657554b3dd5a054caf172e84f
salubrious-lifecycle-8559 <williamsaulon24@gmail.com>
2020-04-08 07:00:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  console.log(animal);
+  console.log(myAnimal);
 }
 
 function add2(n) {
